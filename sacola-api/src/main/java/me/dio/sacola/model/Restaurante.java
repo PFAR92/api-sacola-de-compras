f112d41fbb74f831e26242bef1e9f86f348315e7
PFAR92 <paulofelipetj@hotmail.com>
2022-09-28 16:12:34 -0300

    28/09/22 16:12

--- sacola-api/src/main/java/me/dio/sacola/model/Restaurante.java
+++ sacola-api/src/main/java/me/dio/sacola/model/Restaurante.java
@@ -1,5 +1,21 @@
 package me.dio.sacola.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embedded;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 public class Restaurante {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String nome;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Produto> cardapio;
+    @Embedded
+    private Endereco endereco;
 }
