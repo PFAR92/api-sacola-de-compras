e74e1f009b3aa73b3ea44293b048aaed7de120a1
PFAR92 <paulofelipetj@hotmail.com>
2022-09-28 16:09:32 -0300

    28/09/22 16:09

--- sacola-api/src/main/java/me/dio/sacola/model/Sacola.java
+++ sacola-api/src/main/java/me/dio/sacola/model/Sacola.java
@@ -1,5 +1,32 @@
 package me.dio.sacola.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import me.dio.sacola.enumeration.FormaPagamento;
+
 public class Sacola {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JsonIgnore
+    private Cliente cliente;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Item> items;
+    private Double valorTotal;
+
+    @Enumerated
+    private FormaPagamento FormaPagamento;
+    private boolean fechada;   
 }
