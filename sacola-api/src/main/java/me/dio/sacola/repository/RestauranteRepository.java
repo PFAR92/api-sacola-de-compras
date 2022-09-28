cf64695c04e6ddca9da3e6be757f3f3ce8a2fdbc
PFAR92 <paulofelipetj@hotmail.com>
2022-09-28 18:12:24 -0300

    28/09/22 18:12

---
+++ sacola-api/src/main/java/me/dio/sacola/repository/RestauranteRepository.java
@@ -0,0 +1,5 @@
+package me.dio.sacola.repository;
+
+public interface RestauranteRepository {
+    
+}
--- sacola-api/src/main/resources/data.sql
+++ sacola-api/src/main/resources/data.sql
@@ -1,14 +1,14 @@
-INSERT INTO restaurante (id, cep, complemento nome) VALUES
+INSERT INTO restaurante (id, cep, complemento, nome) VALUES
 (1l, '0000001', 'Complemento Endereço Restaurante 1', 'Restaurante 1'),
-(2l, '0000002', 'Complemento Endereço Restaurante 2', 'Restaurante 2'),
+(2l, '0000002', 'Complemento Endereço Restaurante 2', 'Restaurante 2');
 
 INSERT INTO cliente (id, cep, complemento, nome) VALUES
-(1l, '0000001', 'Complemento Endereço Cliente 1', 'Cliente 1'),
+(1L, '0000001', 'Complemento Endereco Cliente 1', 'Cliente 1');
 
 INSERT INTO produto (id, disponivel, nome, valor_unitario, restaurante_id) VALUES
-(1l, true, 'Produto 1', 5.0, 1l),
-(2l, true, 'Produto 2', 6.0, 1l),
-(3l, true, 'Produto 3', 7.0, 2l),
+(1L, true, 'Produto 1', 5.0, 1L),
+(2L, true, 'Produto 2', 6.0, 1L),
+(3L, true, 'Produto 3', 7.0, 2L);
 
 INSERT INTO sacola (id, forma_pagamento, fechada, valor_total, cliente_id) VALUES
-(1l, 0, false, 0.0, 1l);
+(1L, 0, false, 0.0, 1l);
