c4c5cc21b0c8e8767f4658242d44ead5c6c1489c
PFAR92 <paulofelipetj@hotmail.com>
2022-09-28 17:17:03 -0300

    28/09/22 17:16

--- sacola-api/src/main/java/me/dio/sacola/model/Endereco.java
+++ sacola-api/src/main/java/me/dio/sacola/model/Endereco.java
@@ -1,6 +1,19 @@
 package me.dio.sacola.model;
 
+import javax.persistence.Embeddable;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Embeddable
+@NoArgsConstructor
 public class Endereco {
+    
     private String cep;
     private String complemento;
 }
--- sacola-api/src/main/java/me/dio/sacola/model/Restaurante.java
+++ sacola-api/src/main/java/me/dio/sacola/model/Restaurante.java
@@ -4,11 +4,25 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Embedded;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@NoArgsConstructor
 public class Restaurante {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
