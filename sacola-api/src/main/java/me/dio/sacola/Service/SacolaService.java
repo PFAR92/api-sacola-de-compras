11df5aa5a5e542902f3bda90c654e68b2eb92991
PFAR92 <paulofelipetj@hotmail.com>
2022-09-29 14:46:33 -0300

    29/09/22 14:46

--- sacola-api/src/main/java/me/dio/sacola/Service/SacolaService.java
+++ sacola-api/src/main/java/me/dio/sacola/Service/SacolaService.java
@@ -2,9 +2,10 @@ package me.dio.sacola.Service;
 
 import me.dio.sacola.model.Item;
 import me.dio.sacola.model.Sacola;
+import me.dio.sacola.resource.dto.ItemDto;
 
 public interface SacolaService {
-    Item incluirItemNaSacola();
+    Item incluirItemNaSacola(ItemDto itemDto);
     Sacola verSacola(Long id);
     Sacola fecharSacola(Long id, int formaPagamento);
 }
---
+++ sacola-api/src/main/java/me/dio/sacola/resource/dto/ItemDto.java
@@ -0,0 +1,5 @@
+package me.dio.sacola.resource.dto;
+
+public class ItemDto {
+
+}
