77b2816d25ea5dcd80eb2a0bf2106a4433e9baa4
PFAR92 <paulofelipetj@hotmail.com>
2022-09-29 15:24:16 -0300

    29/09/22 15:24

---
+++ sacola-api/src/main/java/me/dio/sacola/Service/SacolaServiceImpl.java
@@ -0,0 +1,5 @@
+package me.dio.sacola.Service;
+
+public class SacolaServiceImpl {
+    
+}
--- sacola-api/src/main/java/me/dio/sacola/resource/SacolaResource.java
+++ sacola-api/src/main/java/me/dio/sacola/resource/SacolaResource.java
@@ -1,10 +1,12 @@
 package me.dio.sacola.resource;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -30,8 +32,9 @@ public class SacolaResource {
         return sacolaService.verSacola(id);
     }
 
-    public Sacola fecharSacola(Long idSacola, int formaPagamento){
-        return sacolaService.fecharSacola(idSacola, formaPagamento);
+    @PatchMapping("/fecharSacola/{sacolaId}")
+    public Sacola fecharSacola(@PathVariable("sacolaId") Long sacolaId, @RequestParam("formaPagamento") int formaPagamento){
+        return sacolaService.fecharSacola(sacolaId, formaPagamento);
     }
 
 
